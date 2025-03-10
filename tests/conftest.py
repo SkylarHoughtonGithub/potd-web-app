434ee2c8e15dcc0ff2116dac5178c70427d01a98
SkylarHoughtonGithub <SkylarHoughton@Github.com>
2025-03-10 16:59:12 -0500

    ignoring false positives

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,14 +5,16 @@ import sys
 
 import pytest
 
-from app import app as flask_app
-
 # Add the parent directory to sys.path to import your app module
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 
+# pylint: disable=wrong-import-position
+# isort: skip
+from app import app as flask_app
+
 
 @pytest.fixture
-def test_app():
+def app():
     """Create and configure a Flask app for testing."""
     # Set testing config
     flask_app.config.update(
